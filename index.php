e28e4d33f14fb033841d8bf66e235d7c8770908a
CharlesVP15 <youquince@gmail.com>
2022-09-02 18:13:55 -0500

    Deploy Heroku 02/09/2022

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
    
    error_log("Inicio de la APP - CharlsDEV!");
 
-   include('/vendor/autoload.php');
+   include('./vendor/autoload.php');
 
    // Ejecucion de las variables de entorno
    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
    $dotenv->load();
 
-   include('/src/routes/index.routes.php');
+   include('./src/routes/index.routes.php');
